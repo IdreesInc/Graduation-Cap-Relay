695c7fb9a4eb271ed9e00072a776bcdf8f353149
Idrees Hassan <idrees@idreesinc.com>
2021-09-12 18:20:55 -0400

    Create rudimentary socket.io server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hi")
+const io = require("socket.io")(5000);
+
+console.log("Starting Helios Relay!");
+
+io.on("connection", function(socket) {
+  console.log("Client connected");
+  socket.emit("message", {"text": "I love you."});
+});
