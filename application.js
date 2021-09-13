7c5edbb509e6a3a96f998115118722074cf4f180
Idrees Hassan <idrees@idreesinc.com>
2021-09-12 23:27:51 -0400

    Buffer pixel updates before broadcasting

--- application.js
+++ application.js
@@ -3,18 +3,29 @@ const app = express();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 
+let pixelUpdates = [];
+
 console.log("Starting Helios Relay!");
 
+// Initialize Socket.IO server
 io.on("connection", function(socket) {
+  console.log(socket.id + " has connected");
   socket.on("draw", (data) => {
-    console.log("Draw: " + data);
-    io.emit("draw", data);
+      for (let coordinates of data) {
+        pixelUpdates.push(coordinates);
+      }
   }); 
-  console.log("Client connected");
 });
 
+setInterval(() => {
+  if (pixelUpdates.length > 0) {
+    console.log("Broadcasting " + pixelUpdates.length + " pixel updates!");
+    io.emit("draw", pixelUpdates);
+    pixelUpdates = [];
+  }
+}, 100)
 
-// TODO: Ensure this is a secure setup
+// Initialize client webserver
 app.use('/', express.static(__dirname + '/client'));
 
 httpServer.listen(5000, function() {
