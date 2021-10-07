17665bf9fca33f2a53ba580b622b2b05e7c594f9
Idrees Hassan <idrees@idreesinc.com>
2021-10-06 23:43:49 -0400

    Add real-time grad cap connection status indicator

--- application.js
+++ application.js
@@ -4,16 +4,40 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 
 let pixelUpdates = [];
+let graduationCapId = "";
+let graduationCapConnected = false;
 
 console.log("Starting Helios Relay!");
 
 // Initialize Socket.IO server
 io.on("connection", function(socket) {
   console.log(socket.id + " has connected");
+  if (graduationCapConnected) {
+    socket.emit("graduation-cap-connected", "Pre-existing connection");
+  }
+
+  socket.on("graduation-cap-connected", (data) => {
+    console.log("Graduation cap has connected with ID " + socket.id);
+    graduationCapId = socket.id;
+    graduationCapConnected = true;
+    io.emit("graduation-cap-connected", data);
+  });
+
   socket.on("brightness", (data) => {
     console.log("Relaying brightness -> " + data);
     io.emit("brightness", data);
-  }); 
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.id === graduationCapId) {
+      console.log("Graduation cap has disconnected!");
+      this.graduationCapId = "";
+      graduationCapConnected = false;
+      io.emit("graduation-cap-disconnected", "Graduation cap disconnected");
+    } else {
+      console.log("Client socket has disconnected!");
+    }
+  });
 });
 
 // Initialize client webserver
--- client/client-application.js
+++ client/client-application.js
@@ -1,9 +1,24 @@
 const socket = io();
 
+let connectionWarning = document.getElementById("connection");
 let brightnessSlider = document.getElementById("brightness");
+let graduationCapConnected = false;
 
 function init() {
     localStorage.debug = 'socket.io-client:socket'
+
+    socket.on("graduation-cap-connected", (data) => {
+        console.log("Graduation cap has connected!");
+        graduationCapConnected = true;
+        connectionWarning.className = "connected";
+    });
+
+    socket.on("graduation-cap-disconnected", (data) => {
+        console.log("Graduation cap has disconnected!");
+        graduationCapConnected = false;
+        connectionWarning.className = "disconnected";
+    });
+
     brightnessSlider.addEventListener("input", () => {
         socket.emit("brightness", brightnessSlider.value);
     });
